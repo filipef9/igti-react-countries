f3dc99ba9e7fd0dcee3aaa43ae1a2fdaf770b55a
filipef9 <filipef9@hotmail.com>
2021-07-03 06:22:12 +0000

    Importação e utilização dos componentes Headr, Main, TextInput

--- src/pages/ReactCountriesPage.jsx
+++ src/pages/ReactCountriesPage.jsx
@@ -1,18 +1,12 @@
+import Header from '../components/Header';
+import Main from '../components/Main';
+
 const ReactCountriesPage = () => {
   return (
-    <div>
-      <header>
-        <div className="bg-gray-100 mx-auto p-4">
-          <h1 className="text-center font-semibold text-xl">
-            igti-react-countries
-          </h1>
-        </div>
-      </header>
-
-      <main>
-        <div className="container mx-auto p-4"></div>
-      </main>
-    </div>
+    <>
+      <Header>igti-react-countries</Header>
+      <Main>content</Main>
+    </>
   );
 };
 
