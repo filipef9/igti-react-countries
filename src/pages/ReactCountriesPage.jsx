d6caabb5d1dec4aa03e48b995bdbef27542e27c3
filipef9 <filipef9@hotmail.com>
2021-07-03 06:44:48 +0000

    Implementação do campo filtro de países.

--- src/pages/ReactCountriesPage.jsx
+++ src/pages/ReactCountriesPage.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import Header from '../components/Header';
 import Main from '../components/Main';
+import TextInput from '../components/TextInput';
 
 const ReactCountriesPage = () => {
+  const [countryFilter, setCountryFilter] = useState('Brazil');
+  const handleChangeCountryFilter = newCountry => {
+    setCountryFilter(newCountry);
+  };
+
   return (
     <>
       <Header>igti-react-countries</Header>
-      <Main>content</Main>
+      <Main>
+        <TextInput
+          id="countryFilter"
+          label="Informe o nome do país (mínimo 3 caracteres):"
+          value={countryFilter}
+          handleOnChange={handleChangeCountryFilter}
+        />
+      </Main>
     </>
   );
 };
