08d1c9fc6dc50f09f11d6a708e6fea563052c705
filipef9 <filipef9@hotmail.com>
2021-07-03 06:08:00 +0000

    Implementação da página ReactCountriesPage.

---
+++ src/pages/ReactCountriesPage.jsx
@@ -0,0 +1,19 @@
+const ReactCountriesPage = () => {
+  return (
+    <div>
+      <header>
+        <div className="bg-gray-100 mx-auto p-4">
+          <h1 className="text-center font-semibold text-xl">
+            igti-react-countries
+          </h1>
+        </div>
+      </header>
+
+      <main>
+        <div className="container mx-auto p-4"></div>
+      </main>
+    </div>
+  );
+};
+
+export default ReactCountriesPage;
