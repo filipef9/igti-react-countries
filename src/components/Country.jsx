2f9210cc56776a82907474e7ee1f2936e2064f58
filipef9 <filipef9@hotmail.com>
2021-07-04 00:00:33 +0000

    Refactoring para evitar o propdiling.

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import Info from './Info';
 
-const Country = ({ children: country = null }) => {
+const Country = ({
+  children: country = null,
+  isVisited = false,
+  onCountryClick = null,
+}) => {
   if (!country) {
     return <div>Impossível redenrizar o país</div>;
   }
 
-  const { name, capital, region, population, area, flag } = country;
+  const { id, name, capital, region, population, area, flag } = country;
   const demographicDensity = population / area;
 
+  const handleCountryClick = _ => {
+    if (onCountryClick) {
+      onCountryClick(id);
+    }
+  };
+
+  const isVisitedClassName = isVisited ? 'bg-green-100' : '';
+
   return (
-    <div className="border p-2 m-2 flex items-center space-x-2">
+    <div
+      className={`border p-2 m-2 flex items-center space-x-2 cursor-pointer ${isVisitedClassName}`}
+      onClick={handleCountryClick}
+    >
       <img src={flag} alt={name} className="w-48" />
       <ul>
         <li>
--- src/pages/ReactCountriesPage.jsx
+++ src/pages/ReactCountriesPage.jsx
@@ -6,6 +6,7 @@ import TextInput from '../components/TextInput';
 import Countries from '../components/Countries';
 
 import { countries } from '../data/countries.js';
+import Country from '../components/Country';
 
 const MIN_CHARACTERS = 3;
 
@@ -15,13 +16,29 @@ const ReactCountriesPage = () => {
     setCountryFilter(newCountry);
   };
 
+  const [visitedCountries, setVisitedCountries] = useState([]);
+  const toggleVisitedCountry = countryId => {
+    let copyVisitedCountries = [...visitedCountries];
+
+    const countryHasAlreadyBeenVisited =
+      copyVisitedCountries.indexOf(countryId) !== -1;
+    if (countryHasAlreadyBeenVisited) {
+      copyVisitedCountries = copyVisitedCountries.filter(
+        visitedCountryId => visitedCountryId !== countryId
+      );
+    } else {
+      copyVisitedCountries.push(countryId);
+    }
+
+    setVisitedCountries(copyVisitedCountries);
+  };
+
   const filteredCountries =
     countryFilter.trim().length >= MIN_CHARACTERS
       ? countries.filter(({ nameInLowercase }) =>
           nameInLowercase.includes(countryFilter.toLocaleLowerCase())
         )
       : countries;
-  console.log(filteredCountries);
 
   return (
     <>
@@ -34,7 +51,28 @@ const ReactCountriesPage = () => {
           handleOnChange={handleChangeCountryFilter}
         />
 
-        <Countries>{filteredCountries}</Countries>
+        <Countries>
+          <h2 className="text-center font-semibold">
+            {filteredCountries.length} país(es)
+          </h2>
+          <h3 className="text-center font-semibold text-sm">
+            {visitedCountries.length} país(es) visitado(s)
+          </h3>
+
+          {filteredCountries.map(country => {
+            const isVisited = visitedCountries.indexOf(country.id) !== -1;
+
+            return (
+              <Country
+                key={country.id}
+                isVisited={isVisited}
+                onCountryClick={toggleVisitedCountry}
+              >
+                {country}
+              </Country>
+            );
+          })}
+        </Countries>
       </Main>
     </>
   );
